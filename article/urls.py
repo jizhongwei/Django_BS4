7ab9cf6e858906ce760d99c644dd4ff9ba151aff
dick <jzwxiaoniao@163.com>
2019-09-29 16:23:38 +0800

    编写视图函数，增加模版文件，并配置视图函数路由

--- article/urls.py
+++ article/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 
+from . import views
+
 app_name = 'article'
 urlpatterns = [
-
+    path('article-list', views.article_list, name = 'article-list'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import ArticlePost
+
+def article_list(request):
+    articles = ArticlePost.objects.all()
+    context = {'articles': articles}
+    return render(request, 'article/list.html', context)
