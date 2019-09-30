7e454011e9061cbb2e2a704f274b8e9b96c462bd
dick <jzwxiaoniao@163.com>
2019-09-30 12:15:55 +0800

    文章详情页视图函数实现markdown语法支持，并且利用pygment包实现代码高亮显示

--- article/views.py
+++ article/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from .models import ArticlePost
+import markdown
+
 
 def article_list(request):
     articles = ArticlePost.objects.all()
@@ -9,5 +11,13 @@ def article_list(request):
 
 def article_detail(request,id):
     article = ArticlePost.objects.get(id = id)
+    #将markdown语法渲染成HTML样式
+    article.body = markdown.markdown(article.body,
+                                     extensions = [
+                                         #包含 缩写、表格等常用扩展
+                                         'markdown.extensions.extra',
+                                         # 语法高亮扩展
+                                         'markdown.extensions.codehilite',
+                                     ])
     context = {'article': article}
     return render(request, 'article/detail.html', context)
