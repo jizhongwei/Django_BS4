8089e02f913a746a946cb126d5fc94a842774c28
dick <jzwxiaoniao@163.com>
2019-09-30 14:10:26 +0800

    利用Django的form表单组件，实现从前端界面写文章，完成处理写文章的视图

--- article/urls.py
+++ article/urls.py
@@ -6,4 +6,5 @@ app_name = 'article'
 urlpatterns = [
     path('article-list', views.article_list, name = 'article-list'),
     path('article-detail/<int:id>/', views.article_detail, name = 'article-detail'),
+    path('article-create/', views.article_create, name = 'article-create'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
+from .forms import ArticlePostForm
+from django.contrib.auth.models import User
 from .models import ArticlePost
 import markdown
 
@@ -21,3 +24,33 @@ def article_detail(request,id):
                                      ])
     context = {'article': article}
     return render(request, 'article/detail.html', context)
+
+#写文章的视图
+def article_create(request):
+    #判断用户是否提交数据
+    if request.method == 'POST':
+        # 将提交的数据赋值到表单实例中
+        article_post_form = ArticlePostForm(data = request.POST)
+        # 判断提交的数据是满足模型的要求
+        if article_post_form.is_valid():
+            # 保存数据，但暂时不提交到数据库中
+            new_article = article_post_form.save(commit= False)
+            # 指定数据库中id=1的用户为作者
+            # 如果你进行过删除数据库的操作，可能会找不到id=1的用户
+            # 此时请重新创建用户，并传入相应的用户id
+            new_article.author = User.objects.get(id = 1)
+            # 将新文章保存到数据库中
+            new_article.save()
+            # 完成后返回到文章列表
+            return redirect('article:article-list')
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse('表单内容有误，请重新填写。')
+    # 如果用户请求获取数据
+    else:
+        # 创建一个空的表单实例
+        article_post_form = ArticlePostForm()
+        # 上下文，传到模版中
+        context = {'article_post_form': article_post_form}
+        # 进行模版渲染
+        return render(request, 'article/create.html', context)
